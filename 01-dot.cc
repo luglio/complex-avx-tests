884c01c936751ac42714b15518c6933a6fc2bac6
luglio <wenzhongz@smu.edu>
2020-02-03 12:57:39 -0600

    impl dot test

--- 01-dot.cc
+++ 01-dot.cc
@@ -1,5 +1,4 @@
 #include "00-util.hpp"
-using namespace std;
 
 cd dot_avx(const cd *vec1, const cd *vec2, const int len)
 {
@@ -66,4 +65,107 @@ cd dot(const cd *vec1, const double *vec2, const int len)
   cd res{0.0, 0.0};
   for (auto j = 0; j < len; ++j) res += vec1[j] * vec2[j];
   return res;
+}
+
+
+void test_c_c(int len, int nloop)
+{
+  // init
+  auto vec1 = ALLOC<cd>(len);
+  auto vec2 = ALLOC<cd>(len);
+  for (auto j = 0; j < len; ++j)
+  {
+    vec1[j] = cd{0.01 * j, j - 1.0};
+    vec2[j] = cd{j - 1.0, 0.01 * j};
+  }
+  for (auto r = 0; r < nloop; ++r)
+  {
+    vec2[r] = cd{0.0, 0.4 * r};
+    vec1[r] = cd{-0.4 * r, 0.0};
+  }
+  cd res;
+  std::cout << "complex-complex vector dot" << std::endl;
+  SLEEP(1000);
+  auto t0 = CLOCK::now();
+  for (auto r = 0; r < nloop; ++r)
+  {
+    // prevent further optimization from the compiler
+    vec1[r] = cd{0.0, 0.4 * r};
+    vec2[r] = cd{-0.4 * r, 0.0};
+    res = dot_avx(vec1, vec2, len);
+  }
+  auto t1 = CLOCK::now();
+  auto el1 = ELAPSED(t1, t0);
+  std::cout << "AVX:     " << el1 << std::endl;
+  // output once so that the for loop is not skipped
+  std::cout << res << std::endl;
+  SLEEP(1000);
+  auto t2 = CLOCK::now();
+  for (auto r = 0; r < nloop; ++r)
+  {
+    vec2[r] = cd{0.0, 0.4 * r};
+    vec1[r] = cd{-0.4 * r, 0.0};
+    res = dot(vec1, vec2, len);
+  }
+  auto t3 = CLOCK::now();
+  auto el2 = ELAPSED(t3, t2);
+  std::cout << "non-AVX: " << el2 << std::endl;
+  std::cout << res << std::endl;
+  FREE(vec1);
+  FREE(vec2);
+}
+
+void test_c_d(int len, int nloop)
+{
+  // init
+  auto vec1 = ALLOC<cd>(len);
+  auto vec2 = ALLOC<double>(len);
+  for (auto j = 0; j < len; ++j)
+  {
+    vec1[j] = cd{0.01 * j, 0.1 * j};
+    vec2[j] = -0.1 * j;
+  }
+  for (auto r = 0; r < nloop; ++r)
+  {
+    vec2[r] = 0.2 * r;
+    vec1[r] = cd{-0.6 * r, 0.0};
+  }
+  cd res;
+  std::cout << "complex-double vector dot" << std::endl;
+  SLEEP(1000);
+  auto t0 = CLOCK::now();
+  for (auto r = 0; r < nloop; ++r)
+  {
+    // prevent further optimization from the compiler
+    vec1[r] = cd{0.2 * r, 0.0};
+    vec2[r] = -0.6 * r;
+    res = dot_avx(vec1, vec2, len);
+  }
+  auto t1 = CLOCK::now();
+  auto el1 = ELAPSED(t1, t0);
+  std::cout << "AVX:     " << el1 << std::endl;
+  // output once so that the for loop is not skipped
+  std::cout << res << std::endl;
+  SLEEP(1000);
+  auto t2 = CLOCK::now();
+  for (auto r = 0; r < nloop; ++r)
+  {
+    vec2[r] = 0.2 * r;
+    vec1[r] = cd{-0.6 * r, 0.0};
+    res = dot(vec1, vec2, len);
+  }
+  auto t3 = CLOCK::now();
+  auto el2 = ELAPSED(t3, t2);
+  std::cout << "non-AVX: " << el2 << std::endl;
+  std::cout << res << std::endl;
+  FREE(vec1);
+  FREE(vec2);
+}
+
+int main()
+{
+  std::cout << std::setprecision(15);
+  test_c_c(1000000, 1000);
+  test_c_d(1000000, 1000);
+  return 0;
 }
